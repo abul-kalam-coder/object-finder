0d5ee53f519210632561f809fd4d732072039f54
abul-kalam-coder <75057436+abul-kalam-coder@users.noreply.github.com>
2021-11-23 05:59:14 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,77 @@
-var state="";
-var obj; 
-function setup(){
+var state=false;
+obj=[];
+objects=""; 
+var x;
+function setup()
+{
     var canvas =  createCanvas(540,400);
     canvas.center();
     canvas.parent('canvas');
     video = createCapture(VIDEO);
     video.size(540, 400);
     video.hide();
+   console.log("setup")
+x=video;
 }
 
-function find(){
-    obj=document.getElementById("object_input").value;
-    object_detector=ml5.objectDetector("cocossd",model_loaded());
-    document.getElementById("status").innerHTML="status : Finding Ojects";
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }else
+    {
+        console.log(results);
+        obj=results;
+    }
 }
-function model_loaded(){
+
+
+function find()
+{
+    objects=document.getElementById("object_input").value;
+    objectDetector=ml5.objectDetector("cocossd",model_loaded);
+    document.getElementById("status").innerHTML="status : Finding Ojects";  
+}
+
+
+function model_loaded()
+{
 console.log(obj);
-state="true";
+state=true;
+console.log("model_loaded");
 }
 
-function draw(){
-image(video,0,0,540,400 );
+
+ function speak()
+{
+    var synth=window.speechSynthesis;
+     speak_data=objects+"has been detected";
+     var utterThis= new SpeechSynthesisUtterance(speak_data);
+     synth.speak(utterThis)
+}
+
+
+function draw()
+{   
+    image(video,0,0,540,400 );
+    if(state!=false)
+    {
+        objectDetector.detect(video,gotResults);
+        for(i=0;i<obj.length;i++)
+        {
+            fill("red");
+            percent=floor(obj[i].confidence*100);
+            text(obj[i].label+" "+percent+"%",obj[i].x,obj[i].y);
+            noFill();
+            stroke("red");
+            rect(obj[i].x,obj[i].y,obj[i].width,obj[i].height);
+            if (obj[i].label==objects)
+            {
+                x.stop(speak());
+            }
+       }
+    }
+    
 }
